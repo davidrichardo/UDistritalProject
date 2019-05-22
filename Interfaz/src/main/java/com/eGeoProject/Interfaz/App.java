8786fb27be64310699fe9d6283247fad867fb1b0
davidriccardo <dramirez@facele.cl>
2019-05-21 19:14:47 -0500

    #1 add ExecCommand

--- Interfaz/src/main/java/com/eGeoProject/Interfaz/App.java
+++ Interfaz/src/main/java/com/eGeoProject/Interfaz/App.java
@@ -1,10 +1,5 @@
 package com.eGeoProject.Interfaz;
 
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
-import java.util.Properties;
-
 /**
  * Hello world!
  *
@@ -12,19 +7,13 @@ import java.util.Properties;
 public class App 
 {
     public static void main( String[] args )
-    {
+	{
 //        System.out.println( "Hello World!" );
-        ConnectionSSH connectionSSH = new ConnectionSSH("omega-B6FF.local",22,"root","ufY645%XE*tHHsB");
-        try {
-        	connectionSSH.establishWithPassword();
-        	System.out.println("Me conecte correctamente");
+		ConnectionSSH connectionSSH = new ConnectionSSH("omega-B6FF.local", 22, "root", "ufY645%XE*tHHsB");
 
-		} catch (JSchException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-			e.getMessage();
-			e.getCause();
-		}
-        
-    }
+		connectionSSH.establishWithPassword();
+		connectionSSH.execCommand();
+		System.out.println("Me conecte correctamente");
+	}
+
 }
--- Interfaz/src/main/java/com/eGeoProject/Interfaz/ConnectionSSH.java
+++ Interfaz/src/main/java/com/eGeoProject/Interfaz/ConnectionSSH.java
@@ -1,8 +1,13 @@
 package com.eGeoProject.Interfaz;
 
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConnectionSSH {
@@ -24,9 +29,9 @@ public class ConnectionSSH {
 	}
 
 
-	public Session establishWithPassword() throws JSchException {
+	public Session establishWithPassword(){
 
-		Session session;
+		Session session = null;
 		JSch jsSch = new JSch();
 		Properties config = new Properties();
 		try {
@@ -36,13 +41,51 @@ public class ConnectionSSH {
 			session.setConfig(config);
 			session.connect();
 			System.out.println("Connected");
-			session.disconnect();
 		} catch (JSchException e) {
 			// TODO: handle exception
 			e.getCause();
-			throw e;
+			e.printStackTrace();
 		}
 		return session;
+	}
+	
+	public void execCommand(){
+		
+		try {
+			Session session = establishWithPassword();
+	    	Channel channel=session.openChannel("exec");
+	        ((ChannelExec)channel).setCommand("ls");
+	        channel.setInputStream(null);
+	        ((ChannelExec)channel).setErrStream(System.err);
+	        
+	        InputStream in=channel.getInputStream();
+	        channel.connect();
+	        byte[] tmp=new byte[1024];
+	        while(true){
+	          while(in.available()>0){
+	            int i=in.read(tmp, 0, 1024);
+	            if(i<0)break;
+	            System.out.print(new String(tmp, 0, i));
+	          }
+	          if(channel.isClosed()){
+	            System.out.println("exit-status: "+channel.getExitStatus());
+	            break;
+	          }
+	          try{Thread.sleep(1000);}catch(Exception ee){}
+	        }
+	        channel.disconnect();
+			
+		} catch (JSchException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			e.getCause();
+		}
+		 catch (IOException e) {
+			// TODO: handle exception
+			 e.printStackTrace();
+			 e.getCause();
+		}
+
 	}
 
 	public String getSshPassword() {
